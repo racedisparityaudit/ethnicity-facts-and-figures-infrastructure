869db75724f2ef5fd53d3bbc1deef03e50fdc72b
Frankie Roberto <frankie@frankieroberto.com>
2019-06-18 09:37:13 +0100

    Remove infrastructure policy attachments

--- accounts/group_policy_attachments.tf
+++ accounts/group_policy_attachments.tf
@@ -7,23 +7,3 @@ resource "aws_iam_group_policy_attachment" "all_users_manage_mfa" {
   group      = "${aws_iam_group.all_users.name}"
   policy_arn = "${aws_iam_policy.ManageMFA.arn}"
 }
-
-resource "aws_iam_group_policy_attachment" "infrastructure_secrets_manager" {
-  group      = "${aws_iam_group.infrastructure.name}"
-  policy_arn = "${data.aws_iam_policy.SecretsManagerReadWrite.arn}"
-}
-
-resource "aws_iam_group_policy_attachment" "infrastructure_s3_full_access" {
-  group      = "${aws_iam_group.infrastructure.name}"
-  policy_arn = "${data.aws_iam_policy.AmazonS3FullAccess.arn}"
-}
-
-resource "aws_iam_group_policy_attachment" "infrastructure_cloudfront_full_access" {
-  group      = "${aws_iam_group.infrastructure.name}"
-  policy_arn = "${data.aws_iam_policy.CloudFrontFullAccess.arn}"
-}
-
-resource "aws_iam_group_policy_attachment" "infrastructure_route53_full_access" {
-  group      = "${aws_iam_group.infrastructure.name}"
-  policy_arn = "${data.aws_iam_policy.AmazonRoute53FullAccess.arn}"
-}
