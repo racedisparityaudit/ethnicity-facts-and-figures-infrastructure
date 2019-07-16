3fdff9ba550bfc63b2fb718ca182f93e651de141
Frankie Roberto <frankie@frankieroberto.com>
2019-07-16 13:23:17 +0100

    Add security auditor user

--- accounts/iam_users.tf
+++ accounts/iam_users.tf
@@ -17,3 +17,8 @@ resource "aws_iam_user" "deploy_static_site_staging" {
   name          = "deploy_static_site_staging"
   force_destroy = false
 }
+
+resource "aws_iam_user" "security_auditor" {
+  name          = "security_auditor"
+  force_destroy = true # Required to destroy users where MFA/other resources aren't managed by terraform - like us
+}
