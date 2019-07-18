ae44cfe83983125fab020748ff80ff707da4d9c9
Samuel Williams <samuelhwilliams@users.noreply.github.com>
2019-07-18 09:12:52 +0100

    Revert "Add security auditor user (#2)"
    This reverts commit 2aedcd1d350feb251c3bc9ac8b07a18ed504c62f.

--- accounts/output.tf
+++
@@ -1,7 +0,0 @@
-output "security_auditor_key_id" {
-  value = aws_iam_access_key.security_auditor.id
-}
-
-output "security_auditor_key_secret" {
-  value = aws_iam_access_key.security_auditor.secret
-}
